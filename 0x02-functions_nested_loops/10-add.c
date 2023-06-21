be9a2eb6bbf9a1e4cf36c0b6a80de52800ab0207
Faith Mwende <faimmwende@gmail.com>
2023-06-21 01:25:02 -0700

    Add two integers and return the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - Adds two integers and returns the result
- * @num1 - Fist integer to be considered
- * @num2 - Second integer to be considered
+ * @num1: - Fist integer to be considered
+ * @num2:- Second integer to be considered
  * Return: Result
  */
 int add(int num1, int num2)
