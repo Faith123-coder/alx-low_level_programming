167cc2320570f5c133e36922803fd7975cb0a4d0
Faith <faimmwende@gmail.com>
2023-06-26 09:49:04 +0300

    Take a pointer to an int and update the value it points to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter and updates value to 98
+ * reset_to_98 - a function that takes a pointer to an int as parameter
  * @n: input
  * Return: n
  */
