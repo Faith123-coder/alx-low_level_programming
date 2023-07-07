a02a9336ae8bb0155da6ef5165be003b34b367ea
Faith <faimmwende@gmail.com>
2023-07-07 12:23:58 +0300

    Static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * _puts - Used to print the sentence
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
