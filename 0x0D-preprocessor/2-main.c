cea9dfbb78b9d4bd5bf2b1cc32fd735fce7da504
Faith Mwende <faimmwende@gmail.com>
2023-07-17 15:15:21 +0300

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Prints the name of the source file
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Name of the source file: %s\n", _FILE_);
+	return (0);
+}
